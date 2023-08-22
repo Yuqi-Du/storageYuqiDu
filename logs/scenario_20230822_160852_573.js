fb11c217d61b6f22fce2c12b9288955a772dd30a
Yuqi Du <istimdu@gmail.com>
2023-08-22 16:12:56 -0700

    workload

---
+++ logs/scenario_20230822_160852_573.js
@@ -0,0 +1,12 @@
+scenario.run({
+    'driver':       'http',
+    'tags':         'block:schema',
+    'threads':      '1',
+    'docscount':    '50',
+    'auth_token':   'AstraCS:OTCvFjqTgAIaYWBQcCUCymau:9b047725e66c58325f34df3d88cf1b2bcafaeaaccf9fd0c38c1c86cac5770549',
+    'jsonapi_port': '443',
+    'protocol':     'https',
+    'path_prefix':  '/api/json',
+    'workload':     'vector-test-to-astra',
+    'alias':        'vectortesttoastra_default_schema'
+});
