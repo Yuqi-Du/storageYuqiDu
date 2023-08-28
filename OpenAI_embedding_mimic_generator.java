3de2849e23afaf312c3c9d0fef121bd8cd70b4a7
Yuqi Du <istimdu@gmail.com>
2023-08-28 11:05:46 -0700

    workload

--- OpenAI_embedding_mimic_generator.java
+++ OpenAI_embedding_mimic_generator.java
@@ -9,22 +9,30 @@ public class OpenAI_embedding_mimic_generator {
         String filename = "vector-dataset.txt";
         int numLines = 1000;
         int numFloatsPerLine = 1536;
+
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+
             Random random = new Random();
+
             for (int i = 0; i < numLines; i++) {
-                StringBuilder line = new StringBuilder();
+                StringBuilder line = new StringBuilder("[");
 
                 for (int j = 0; j < numFloatsPerLine; j++) {
                     float floatValue = random.nextFloat() * 2 - 1; // Generates float between -1 and 1
                     line.append(floatValue);
+
                     if (j < numFloatsPerLine - 1) {
                         line.append(",");
                     }
                 }
+
+                line.append("]");
+
                 writer.write(line.toString());
                 writer.newLine();
             }
+
             writer.close();
             System.out.println("File generated successfully: " + filename);
         } catch (IOException e) {
